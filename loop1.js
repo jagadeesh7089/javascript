2174957473cc72abde2247934b1ddb28b7ef04d0
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-03-23 19:08:01 +0530

    javascript_Assignment

---
+++ loop1.js
@@ -0,0 +1,49 @@
+// var a=1;
+// while(a<=10)
+// {
+//     console.log(a)
+//     a++;
+// }
+
+// var a=5;
+// while(a<=10)
+// {
+//     console.log(a)
+//     a++;
+// }
+
+
+// var a=-10;
+// while(a<=10)
+// {
+//     console.log(a)
+//     a++;
+// }
+
+// var a=-45;
+// while(a<=-25)
+// {
+//     console.log(a)
+//     a++;
+// }
+
+var a=10;
+while(a>=1)
+{
+  console.log(a) 
+  a--;
+}
+
+var a=-2;
+while(a>=-18)
+{
+    console.log(a)
+    a--;
+}  
+
+var a=10;
+while(a>=-10)
+{
+    console.log(a)
+    a++;
+}
