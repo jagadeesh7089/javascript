6245cbfe91e278c9bc10f196d084201e903827a5
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-05-17 12:33:22 +0530

    ddd

---
+++ items.js
@@ -0,0 +1,154 @@
+
+var items={
+    starters:[
+        
+        {
+            itemname:"Veg-manchuria",
+            itemprice:700,
+            quantity:1,
+            quantityunits:"kg",
+
+        },
+        {
+            itemname:" chicken-lollipop",
+            itemprice:1000,
+            quantity:1,
+            quantityunits:"kg",
+
+        },
+        {
+            itemname:" chicken-65",
+            itemprice:1200,
+            quantity:1,
+            quantityunits:"kg",
+
+        },
+        {
+            itemname:" panner",
+            itemprice:1700,
+            quantity:1,
+            quantityunits:"kg",
+
+        },
+    ],
+    liquids:[
+        {
+            itemname:"Sambar",
+            itemprice:500,
+            quantity:1,
+            quantityunits:"liter",
+
+        },
+        {
+            itemname:"Rasam",
+            itemprice:300,
+            quantity:1,
+            quantityunits:"liter",
+
+        },
+        {
+            itemname:"curd",
+            itemprice:700,
+            quantity:1,
+            quantityunits:"liter",
+
+        },
+        {
+            itemname:" punukulu-pulusu",
+            itemprice:1700,
+            quantity:1,
+            quantityunits:"liter",
+
+        },
+    ],
+    curies:[
+        {
+            itemname:" Tamato-dal",
+            itemprice:1000,
+            quantity:1,
+            quantityunits:"kg",
+
+        },
+        {
+            itemname:"Mulakada",
+            itemprice:700,
+            quantity:1,
+            quantityunits:"kg",
+
+        },
+        {
+            itemname:" Potato-fry",
+            itemprice:500,
+            quantity:1,
+            quantityunits:"kg",
+
+        },
+        {
+            itemname:" Chicken",
+            itemprice:1300,
+            quantity:1,
+            quantityunits:"kg",
+
+        },
+    ],
+    rice:[
+        {
+            itemname:" Pulihora",
+            itemprice:700,
+            quantity:1,
+            quantityunits:"kg",
+
+        },
+        {
+            itemname:"Tamato-rice",
+            itemprice:600,
+            quantity:1,
+            quantityunits:"kg",
+
+        },
+        {
+            itemname:" Biriyani",
+            itemprice:1700,
+            quantity:1,
+            quantityunits:"kg",
+
+        },
+        {
+            itemname:"Rice",
+            itemprice:500,
+            quantity:1,
+            quantityunits:"kg",
+
+        },
+    ],
+    drinks:[
+        {
+            itemname:"Mocktails",
+            itemprice:700,
+            quantity:1,
+            quantityunits:"liter",
+
+        },
+        {
+            itemname:"cooldrinks",
+            itemprice:200,
+            quantity:1,
+            quantityunits:"liter",
+
+        },
+        {
+            itemname:"Rasana",
+            itemprice:300,
+            quantity:1,
+            quantityunits:"liter",
+
+        },
+        {
+            itemname:"Fruit-juice",
+            itemprice:1200,
+            quantity:1,
+            quantityunits:"liter",
+
+        },
+    ],
+}
